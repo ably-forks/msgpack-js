37eedee6c899622b662264d1adf7fbfd2d99d6c7
Paddy Byers <paddy.byers@gmail.com>
2017-01-06 00:38:23 +0000

    More compact implementation of typedarray version

--- test.js
+++ test.js
@@ -1,5 +1,4 @@
 "use strict";
-var bops = require('bops');
 var test = require('tape');
 var msgpackBuffer = require('./msgpack-buffer');
 var msgpackTypedArray = require('./msgpack-typedarray');
@@ -7,7 +6,7 @@ var msgpackBops = require('./msgpack-bops');
 var util = require('util');
 
 var tests = [
-  true, false, null, undefined,
+  true, false, null,
   0, 1, -1, 2, -2, 4, -4, 6, -6,
   0x10, -0x10, 0x20, -0x20, 0x40, -0x40,
   0x80, -0x80, 0x100, -0x100, 0x200, -0x100,
@@ -15,7 +14,8 @@ var tests = [
   0x20000, -0x20000, 0x40000,-0x40000,
   10, 100, 1000, 10000, 100000, 1000000,
   -10, -100, -1000, -10000, -100000, -1000000,
-  'hello', 'world', bops.from("Hello"), bops.from("World"),
+  'hello', 'world',
+  Buffer.from('Hello'), Buffer.from('World'),
   [1,2,3], [], {name: "Tim", age: 29}, {},
   {a: 1, b: 2, c: [1, 2, 3]}
 ];
@@ -26,42 +26,34 @@ test('encode to buffer works as expected', function(assert) {
     var packed = msgpackBuffer.encode(input);
     console.log(packed);
     var output = msgpackBops.decode(packed);
-    if (bops.is(input)) {
-      assert.true(bops.is(output));
-      for (var i = 0, l = input.length; i < l; i++) {
-        assert.equal(input[i], output[i]);
-      }
-      assert.equal(input.length, output.length);
+    if(Buffer.isBuffer(input)) {
+      assert.true(Buffer.isBuffer(output));
+      assert.true(input.compare(output) === 0);
     }
     else {
       assert.deepEqual(input, output);
     }
   });
-
   assert.end();
-
 });
 
 test('encode to typedarray works as expected', function(assert) {
 
   tests.forEach(function (input) {
-    var packed = msgpackTypedArray.encode(input);
-    console.log(packed);
-    var output = msgpackBops.decode(Buffer.from(packed));
-    if (bops.is(input)) {
-      assert.true(bops.is(output));
-      for (var i = 0, l = input.length; i < l; i++) {
-        assert.equal(input[i], output[i]);
-      }
-      assert.equal(input.length, output.length);
-    }
-    else {
+    if(Buffer.isBuffer(input)) {
+      var inputAsArrayBuffer = input.buffer.slice(input.byteOffset, input.byteOffset + input.length);
+      var packed = msgpackTypedArray.encode(inputAsArrayBuffer);
+      console.log(packed);
+      var output = msgpackBops.decode(Buffer.from(packed));
+      assert.true(input.compare(output) === 0);
+    } else {
+      var packed = msgpackTypedArray.encode(input);
+      console.log(packed);
+      var output = msgpackBops.decode(Buffer.from(packed));
       assert.deepEqual(input, output);
     }
   });
-
   assert.end();
-
 });
 
 test('decode from buffer works as expected', function(assert) {
@@ -72,40 +64,43 @@ test('decode from buffer works as expected', function(assert) {
     var output = msgpackBuffer.decode(packed);
     if (Buffer.isBuffer(input)) {
       assert.true(Buffer.isBuffer(output));
-      for (var i = 0, l = input.length; i < l; i++) {
-        assert.equal(input[i], output[i]);
-      }
-      assert.equal(input.length, output.length);
+      assert.true(input.compare(output) === 0);
     }
     else {
       assert.deepEqual(input, output);
     }
   });
-
   assert.end();
-
 });
 
-test('decode from typedarray works as expected', function(assert) {
+test('decode from typedarray works as expected (to ArrayBuffers)', function(assert) {
 
   tests.forEach(function (input) {
     var packed = msgpackBops.encode(input);
-    console.log(packed);
-    var output = msgpackTypedArray.decode(packed);
-    if (input instanceof Uint8Array) {
-      assert.true(output instanceof Uint8Array);
-      for (var i = 0, l = input.length; i < l; i++) {
-        assert.equal(input[i], output[i]);
-      }
-      assert.equal(input.length, output.length);
-    }
-    else {
+    var packedAsArrayBuffer = packed.buffer.slice(packed.byteOffset, packed.byteOffset + packed.length);
+    var output = msgpackTypedArray.decode(packedAsArrayBuffer);
+    if(Buffer.isBuffer(input)) {
+      assert.true(input.compare(Buffer.from(output)) === 0);
+    } else {
       assert.deepEqual(input, output);
     }
   });
-
   assert.end();
+});
 
+test('decode from typedarray works as expected (to node Buffers)', function(assert) {
+
+  tests.forEach(function (input) {
+    var packed = msgpackBops.encode(input);
+    var packedAsArrayBuffer = packed.buffer.slice(packed.byteOffset, packed.byteOffset + packed.length);
+    var output = msgpackTypedArray.decode(packedAsArrayBuffer, 0, 0, true);
+    if(Buffer.isBuffer(input)) {
+      assert.true(input.compare(output) === 0);
+    } else {
+      assert.deepEqual(input, output);
+    }
+  });
+  assert.end();
 });
 
 function Foo () {
